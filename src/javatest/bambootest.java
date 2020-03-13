6856e5aeb42ee019e028506cb067f415a5c72858
Muhammad Umar Butt <Umar.Butt@Cdocs.local>
2020-03-13 18:36:16 +0500

    test

--- src/javatest/bambootest.java
+++ src/javatest/bambootest.java
@@ -1,26 +1,31 @@
 package javatest;
+//
+//import org.junit.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import junit.framework.Assert;
+//import junit.framework.Assert;
 
 public class bambootest {
 	
-	@Test	
-	public void test()  {
-		Assert.assertEquals("abc", "abc");
-		
-//	  System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-//	  WebDriver driver = new ChromeDriver();
-//	  driver.manage().window().maximize();
-//	  driver.get("http://facebook.com");
-//	  Thread.sleep(2000);
-}
-	@Test
-	public void test2()
+	public static void main(String args[])
 	{
-		Assert.assertEquals("abc", "qwr");
+		System.out.print("testttttttttttttttt");
 	}
+	
+//	@Test	
+//	public void test()  {
+//		Assert.assertEquals("abc", "abc");
+//		
+////	  System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+////	  WebDriver driver = new ChromeDriver();
+////	  driver.manage().window().maximize();
+////	  driver.get("http://facebook.com");
+////	  Thread.sleep(2000);
+//}
+//	@Test
+//	public void test2()
+//	{
+//		Assert.assertEquals("abc", "qwr");
+//	}
 }
